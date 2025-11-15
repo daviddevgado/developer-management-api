5451f65fd3e4e57c270175b5cf8b25bc6c3380dc
daviddevgado <232341122+daviddevgado@users.noreply.github.com>
2025-11-15 17:36:48 +0000

    Initial commit: Developer Management API with Spring Boot

---
+++ src/main/java/com/daviddevgado/developer_management_api/DeveloperManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.daviddevgado.developer_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeveloperManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeveloperManagementApiApplication.class, args);
+	}
+
+}
